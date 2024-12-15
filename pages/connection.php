4e6c5828969bfa6a3eb3150a8a67dfd63d61574d
four-J-MAX <cuevaj186@gmail.com>
2024-12-15 23:10:02 +0800

    commit

--- pages/connection.php
+++ pages/connection.php
@@ -1,13 +1,17 @@
-<?php		
-			// Establishing Connection with Server by passing inputs as a parameter
+<?php
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+// Establishing Connection with Server by passing inputs as a parameter
+$con = mysqli_connect('127.0.0.1', 'u510162695_barangay', '1Db_barangay', 'u510162695_barangay');
 
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
- $con = mysqli_connect('127.0.0.1','u510162695_barangay','1Db_barangay','u510162695_barangay') or die(mysqli_error());
-
-
-
-			date_default_timezone_set("Asia/Manila");
-
-
+// Set the default timezone
+date_default_timezone_set("Asia/Manila");
 ?>
